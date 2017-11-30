475ca017f7a9c84631d92c6942d759647cd803fa
Louis Pazos <louispazos@gmail.com>
2017-11-30 18:46:49 +0000

    added code to show headlines

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*global $ APIKEY*/
+/*global $ APIKEY p*/
 $(document).ready(function() {
     $.ajax({
             // method: "GET ",
@@ -27,6 +27,25 @@ $(document).ready(function() {
         
         $('#source').submit( function(event) {
             event.preventDefault();
-            alert(document.getElementById("selection").value)
+            // alert(document.getElementById("selection").value)
+            
+            $.ajax({
+                
+                method: "GET",
+                url: "https://newsapi.org/v2/top-headlines",
+                data: {sources: "bbc-news", apiKey: APIKEY },
+                success: function(data) {
+                if(data.status === "ok") {
+                    console.log(data)
+                    
+                    for (var i = 0; i < data.articles.length; i++) {
+                        var headline = document.createElement(p);
+                        
+                        headline.innerHTML = data.articles[i].title;
+                        document.getElementById("buss").appendChild(headline)
+                    }
+                }
+                 }
+            })
         });
 })
